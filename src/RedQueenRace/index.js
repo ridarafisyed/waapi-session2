57c8f3aff2d0fed7ea4e65f011b19009dace8589
Rida R Syed <ridarafisyed@yahoo.com>
2020-07-19 22:55:58 +0500

    Update index.js

--- src/RedQueenRace/index.js
+++ src/RedQueenRace/index.js
@@ -1,10 +1,16 @@
 /** @format */
 
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 import useWebAnimations from "@wellyshen/use-web-animations";
 
 export const RedQueenRace = () => {
+	var playbackrateRQ = 1;
+	var playbackrateBG = 0;
+
+	// const animations= React.useRef(null)
+	// const anime = useWebAnimations(animations)
+
 	/* Background animations */
 	// var sceneryFrames = [
 	//     { transform: 'translateX(100%)' },
@@ -27,11 +33,13 @@ export const RedQueenRace = () => {
 	const sceneryTimingBackground = {
 		duration: 36000,
 		iterations: Infinity,
+		playbackRate: playbackrateBG,
 	};
 
 	const sceneryTimingForeground = {
 		duration: 12000,
 		iterations: Infinity,
+		playbackRate: playbackrateBG,
 	};
 	// var background1 = document.getElementById('background1');
 
@@ -59,6 +67,7 @@ export const RedQueenRace = () => {
 
 	// var foreground1Movement = foreground1.animate(
 	//     sceneryFrames, sceneryTimingForeground);
+
 	// foreground1Movement.currentTime = foreground1Movement.effect.timing.duration / 2;
 
 	const foreground1Movement = useWebAnimations({
@@ -79,6 +88,11 @@ export const RedQueenRace = () => {
 	//     { transform: 'translateY(-100%)' }
 	// ];
 
+	const spriteFrames = [
+		{ transform: "translateY(0)" },
+		{ transform: "translateY(-100%)" },
+	];
+
 	// var redQueen_alice_sprite = document.getElementById('red-queen_and_alice_sprite');
 
 	// var redQueen_alice = redQueen_alice_sprite.animate(
@@ -89,6 +103,17 @@ export const RedQueenRace = () => {
 	//     playbackRate: 1,
 	//     iterations: Infinity
 	// });
+	const spirteTiming = {
+		easing: "steps(7, end)",
+		direction: "reverse",
+		duration: 600,
+		playbackRate: playbackrateRQ,
+		iterations: Infinity,
+	};
+	const redQueen_alice = useWebAnimations({
+		keyframes: spriteFrames,
+		timing: spirteTiming,
+	});
 
 	// /* Alice tires so easily!
 	//   Every so many seconds, reduce their playback rate so they slow a little.
@@ -110,6 +135,20 @@ export const RedQueenRace = () => {
 	//         });
 	//     }
 	// }
+
+	const adjustBackgroundPlayback = () => {
+		if (playbackrateRQ < 0.8) {
+			playbackrateBG = (playbackrateRQ / 2) * -1;
+		} else if (playbackrateRQ > 1.2) {
+			playbackrateBG = playbackrateRQ / 2;
+		} else {
+			playbackrateBG = 0;
+		}
+		foreground1Movement.getAnimation().playbackRate = playbackrateBG;
+		foreground2Movement.getAnimation().playbackRate = playbackrateBG;
+		background1Movement.getAnimation().playbackRate = playbackrateBG;
+		background2Movement.getAnimation().playbackRate = playbackrateBG;
+	};
 	// adjustBackgroundPlayback();
 
 	// /* If Alice and the Red Queen are running at a speed of 1, the background doesn't move. */
@@ -128,6 +167,29 @@ export const RedQueenRace = () => {
 	//     adjustBackgroundPlayback();
 	// }
 
+	useEffect(() => {
+		const fganimation = foreground1Movement.getAnimation();
+		fganimation.currentTime = fganimation.effect.getTiming().duration / 2;
+
+		const bganimation = background1Movement.getAnimation();
+		bganimation.currentTime = bganimation.effect.getTiming().duration / 2;
+
+		setInterval(() => {
+			/* Set decay */
+			if (playbackrateRQ > 0.4) {
+				playbackrateRQ *= 0.9;
+				redQueen_alice.getAnimation().playbackRate = playbackrateRQ;
+			}
+			adjustBackgroundPlayback();
+		}, 3000);
+
+		document.addEventListener("click", () => {
+			playbackrateRQ *= 1.1;
+			redQueen_alice.getAnimation().playbackRate = playbackrateRQ;
+			adjustBackgroundPlayback();
+		});
+	});
+
 	// document.addEventListener("click", goFaster);
 	// document.addEventListener("touchstart", goFaster);
 
@@ -142,11 +204,12 @@ export const RedQueenRace = () => {
 							src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
 							srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
 							alt="Alice and the Red Queen running to stay in place."
+							ref={redQueen_alice.ref}
 						/>
 					</div>
 				</div>
 
-				<div className="scenery" id="foreground1" ref={foreground1Movement}>
+				<div className="scenery" id="foreground1" ref={foreground1Movement.ref}>
 					<img
 						id="palm3"
 						src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
@@ -154,7 +217,7 @@ export const RedQueenRace = () => {
 						alt=" "
 					/>
 				</div>
-				<div className="scenery" id="foreground2" ref={foreground2Movement}>
+				<div className="scenery" id="foreground2" ref={foreground2Movement.ref}>
 					<img
 						id="bush"
 						src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
@@ -168,7 +231,7 @@ export const RedQueenRace = () => {
 						alt=" "
 					/>
 				</div>
-				<div className="scenery" id="background1">
+				<div className="scenery" id="background1" ref={background1Movement.ref}>
 					<img
 						id="r_pawn_upright"
 						src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
@@ -188,7 +251,7 @@ export const RedQueenRace = () => {
 						alt=" "
 					/>
 				</div>
-				<div className="scenery" id="background2">
+				<div className="scenery" id="background2" ref={background2Movement.ref}>
 					<img
 						id="r_pawn"
 						src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
